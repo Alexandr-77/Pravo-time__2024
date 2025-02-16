37ba7192d77ad82ad6dbf7d40291ef285fd13910
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2025-02-17 00:37:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,105 +1,133 @@
-// 
-// ================================================== 
-let burger = document.querySelector('.burger');
-let nav = document.querySelector('.header__block-nav');
-let liHeader = document.querySelectorAll('.header__menu-item');
+//
+// ==================================================
+let body = document.querySelector('body');
+let itemLiMenu = document.querySelector("#id-item-uslugi");
 
-burger.addEventListener('click', rotateBurger); 
+let burger = document.querySelector(".burger");
+let nav = document.querySelector(".header__block-nav");
+let liHeader = document.querySelectorAll(".header__menu-item");
+let newlink = document.querySelectorAll(".newlink");
 
-for(let i = 0; i < liHeader.length; i++ ) {
-	if (window.innerWidth < 990 ) {
-		liHeader[i].addEventListener('click', rotateBurger);
-	}
-}
+burger.addEventListener("click", rotateBurger);
 
+function rotateBurger() {
+  burger.classList.toggle("burger-active");
+  body.classList.toggle('body-fix');
+  boxSubMenu.style.display = "none";
 
-function rotateBurger() { 
-  burger.classList.toggle('burger-active');
-
-  if (nav.style.display === "block") {
+  if (nav.style.display === "flex") {
     nav.style.display = "none";
   } else {
-    nav.style.display = "block";
+    nav.style.display = "flex";
+  }
+}
+// -------------------- submenu ---------------------------
+let boxSubMenu = document.querySelector(".header__submenu");
+
+itemLiMenu.addEventListener("click", fnSoweMenu);
+
+function fnSoweMenu() {
+  if (boxSubMenu.style.display === "block" ) {
+    boxSubMenu.style.display = "none";
+    // if (window.innerWidth < 990) {
+    //   rotateBurger();
+    // } else {
+    //   none;
+    // }
+  } else {
+    boxSubMenu.style.display = "block";
+  }
+}
+
+for (let i = 0; i < newlink.length; i++) {
+  newlink[i].addEventListener("click", () => {
+    if (boxSubMenu.style.display === "block") {
+      boxSubMenu.style.display = "none";
+    } else {
+      boxSubMenu.style.display = "none";
+    }
+  });
+}
+
+for (let i = 0; i < newlink.length; i++) {
+  if (window.innerWidth < 990) {
+    newlink[i].addEventListener("click", rotateBurger);
   }
 }
 
 
-// ======== swiper ========
 
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	autoplay: 500,
-	slidesPerView: 2.8,
-	spaceBetween: 100,
-	centeredSlides: true, 
-	loop: true,
-	// If we need pagination
-	pagination: {
-	el: '.swiper-pagination',
-	},
-	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-
+// отправка формы =============>
+$(document).ready(function () {
+  //E-mail Ajax Send
+  $("form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Спасибо. Ваша заявка была отправлена.");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 });
-// ======== swiper / ========
+// ============================
+
+
+
+// ============== prise ============ 
+const priceSelect = document.querySelector('#price__select-id');
+const priceBox = document.querySelector('#price__box');
+const priceBox2 = document.querySelector('#price__box2');
+
+priceSelect.addEventListener('change', function name(params) {
+  if (this.value == 1) {
+    priceBox.style.display = 'block';
+    priceBox2.style.display = 'none';
+  }
+  if (this.value == 2) {
+    priceBox2.style.display = 'block';
+    priceBox.style.display = 'none';
+  }
+});
 
 
 
 // ========= slyder slick ==========
-$('.slyder__box').slick({
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	infinite: true,
-	arrow: true,
-	dots: true,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	responsive: [
-		{
-		  breakpoint: 1400,
-		  settings: {
-			 slidesToShow: 2,
-			 slidesToScroll: 1,
-			 infinite: true,
-			 dots: true
-		  }
-		},
-		{
-		  breakpoint: 768,
-		  settings: {
-			 slidesToShow: 1,
-			 slidesToScroll: 1
-		  }
-		}
-	 ]
- });
-
-
-// отправка формы =============>
-$(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Спасибо. Ваша заявка была отправлена.");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
+$(".slyder__box").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  arrow: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
-// ============================
+
 // ===============================================
 
 new WOW().init();
